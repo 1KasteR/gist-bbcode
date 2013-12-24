5fe4ea6d87adae123b2df791682a176fce03a1a5
1KasteR <kaster@yahoo.com>
2013-12-24 14:56:39 -0800

    Create gist_bbcode_include.php

---
+++ includes/bbcodes/gist_bbcode_include.php
@@ -0,0 +1,25 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) 2002 - 2014 Nick Jones
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: gist_bbcode_include.php
+| Author: KasteR
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) { die("Access Denied"); }
+add_to_head('
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/gist-embed/1.6/gist-embed.min.js"></script>
+');
+//Gist bbcode
+$text = preg_replace('#\[gist\](.*?)\[/gist\]#si', '<code data-gist-id="\1"></code>', $text);
+?>
